e018561aab6ba5326398a5a1bc4316c441e0c584
ikolabo <noriyayamaji@gmail.com>
2015-03-04 03:11:32 +0900

    Create common.js

---
+++ common.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  // inputが１つのフォームで、Enter→submitを防ぐため。
+  $("input:not(.allow_submit)").on("keypress", function(){
+    return event.which !== 13;  // 13:Enter
+  });
+
+});
